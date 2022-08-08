ea2189fde11e859b00f9d4663c4f304fe3bed03f
Benjamin <benjaminluwong@gmail.com>
2022-08-08 22:11:53 +1200

    set up basic search engline template and made sure API calls are successful

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios";
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [monsterName, setMonsterName] = useState("");
+
+  const DND_MONSTER_BASE_URL = "https://api.open5e.com"
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        DnD Monster Guide
+      </h1>
+
+      <div>
+        <label> Monster Name </label>
+        <br/>
+        <input 
+        type="text" 
+        id="monster-name" 
+        name="monster-name" 
+        onChange={e => setMonsterName(e.target.value)}/>
+        <br/>
+        <button onClick={search}>
+          Search
+        </button>
+      </div>
+
+      <p>
+        You have entered {monsterName}
+      </p>
     </div>
   );
+
+  function search(){
+    //Have to convert name to lowercase as monster indexes in the API lowercase 
+    axios.get(DND_MONSTER_BASE_URL + "/monsters/" + monsterName.toLowerCase()).then((res) => {
+      console.log(res.data);
+    });
+}
 }
 
 export default App;
