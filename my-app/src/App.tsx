301961310c5fa72db165daf2d10f61c7d423644d
Benjamin <benjaminluwong@gmail.com>
2022-08-09 14:16:56 +1200

    reformated the app.tsx file before working on UI

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import axios from "axios";
+import axios from 'axios';
 import { useState } from 'react';
 import './App.css';
 
 function App() {
 
   const [monsterName, setMonsterName] = useState("");
+  const [monsterInfo, setMonsterInfo] = useState<undefined | any>(undefined);
 
-  const DND_MONSTER_BASE_URL = "https://api.open5e.com"
+  const DND5E_BASE_URL = "https://www.dnd5eapi.co/api/"
+
   return (
-    <div>
+    <div class="content-center align-content: center">
       <h1>
         DnD Monster Guide
       </h1>
@@ -21,7 +23,7 @@ function App() {
         type="text" 
         id="monster-name" 
         name="monster-name" 
-        onChange={e => setMonsterName(e.target.value)}/>
+        onChange={(e) => setMonsterName(e.target.value)}/>
         <br/>
         <button onClick={search}>
           Search
@@ -31,13 +33,31 @@ function App() {
       <p>
         You have entered {monsterName}
       </p>
+
+      {monsterInfo === undefined ? (
+        <p> Monster not found.</p>
+      ) : (
+        <div id="equipment-result">
+          <p>Name: {monsterInfo.name}</p>
+          <p>Challenge Rating: { monsterInfo.challenge_rating }</p>
+          <p>HP: { monsterInfo.hit_points }</p>
+          <p>AC: { monsterInfo.armor_class }</p>
+          <p>Speed: </p>
+          <p>INT: { monsterInfo.intelligence }</p>
+          <p>CON: { monsterInfo.constitution }</p>
+          <p>CHA: { monsterInfo.charisma } </p>
+          <p>DEX: { monsterInfo.dexterity }</p>
+          <p>STR: { monsterInfo.strength } </p>
+          <p>WIS: { monsterInfo.wisdom }</p>
+        </div>
+      )}
     </div>
   );
 
   function search(){
     //Have to convert name to lowercase as monster indexes in the API lowercase 
-    axios.get(DND_MONSTER_BASE_URL + "/monsters/" + monsterName.toLowerCase()).then((res) => {
-      console.log(res.data);
+    axios.get(DND5E_BASE_URL  + "/monsters/" + monsterName.toLowerCase()).then((res) => {
+      setMonsterInfo(res.data);
     });
 }
 }
